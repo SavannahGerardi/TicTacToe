ebc785890ea31d00d6a44662e44a9a1497a9be3b
SavannahGerardi <savannahgerardi@outlook.com>
2023-10-19 22:37:24 -0400

    Add comments and some variables

---
+++ main.py
@@ -0,0 +1,15 @@
+#vars 
+player_one_wins = 0
+player_two_wins = 0
+
+
+#ask user if they are playing alone or with another person
+
+#code if playing alone
+
+#code if playing together
+
+#code if game over
+
+#code who won/lost
+
